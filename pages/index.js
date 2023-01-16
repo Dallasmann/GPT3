c6b12acaaf1f59c6a4380f3d0fb9efc2d51d936a
Dallasmann <122212490+Dallasmann@users.noreply.github.com>
2023-01-16 21:49:28 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,94 @@
-import { useState } from 'react';
-import Head from 'next/head';
+import { useCallback, useEffect, useState } from 'react';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
-
 const Home = () => {
-  const [userInput, setUserInput] = useState('');
-  const [apiOutput, setApiOutput] = useState('')
-  const [isGenerating, setIsGenerating] = useState(false)
-
-const callGenerateEndpoint = async () => {
-  setIsGenerating(true);
-  
-  console.log("Calling OpenAI...")
-  const response = await fetch('/api/generate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ userInput }),
-  });
-
-  const data = await response.json();
-  const { output } = data;
-  console.log("OpenAI replied...", output.text)
-
-  setApiOutput(`${output.text}`);
-  setIsGenerating(false);
-}
-  const onUserChangedText = (event) => {
-  console.log(event.target.value);
-  setUserInput(event.target.value);
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+  const onChange = (event) => {
+    setInput(event.target.value);
   };
+
+  const generateAction = useCallback(async () => {
+    if (isGenerating) return;
+
+    setIsGenerating(true);
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ input }),
+    });
+    const data = await response.json();
+    const { baseChoice, finalChoice } = data;
+    setOutput(
+      `Song Titles:${finalChoice.text}\n\nLyrics:\n${input}${baseChoice.text}`
+    );
+
+    setIsGenerating(false);
+  }, [input, isGenerating]);
+
+  useEffect(() => {
+    const keydownHandler = async (event) => {
+      if ((event.metaKey || event.ctrlKey) && event.code === 'Enter') {
+        event.preventDefault();
+        await generateAction();
+      }
+    };
+
+    window.addEventListener('keydown', keydownHandler);
+
+    return () => {
+      window.removeEventListener('keydown', keydownHandler);
+    };
+  }, [generateAction]);
+
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Tinder catch phrase generator</h1>
+            <h1>GPT-3</h1>
+            <h1>Writer</h1>
           </div>
           <div className="header-subtitle">
-            <h2>tell me more about the person</h2>
+            <h2>Write your first song in the style of your favorite artist.</h2>
           </div>
-          <div className="prompt-container">
-          <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText} />
-           <div className="prompt-buttons">
-          <a className="generate-button" onClick={callGenerateEndpoint}>
-          <div className="generate">
-          <p>Generate</p>
-      </div>
-    </a>
-  </div>
-  {apiOutput && (
-  <div className="output">
-    <div className="output-header-container">
-      <div className="output-header">
-        <h3>Output</h3>
-      </div>
-    </div>
-    <div className="output-content">
-      <p>{apiOutput}</p>
-    </div>
-  </div>
-  )}
         </div>
+        <div className="prompt-container">
+          <textarea className="prompt-box" value={input} onChange={onChange} />
+          <div className="prompt-buttons">
+            <div className="key-stroke">
+              <p>cmd/ctrl + enter</p>
+            </div>
+            <div className="or">
+              <p>OR</p>
+            </div>
+            <a
+              className={
+                isGenerating ? 'generate-button loading' : 'generate-button'
+              }
+              onClick={generateAction}
+            >
+              <div className="generate">
+                {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
         </div>
+        {output && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{output}</p>
+            </div>
+          </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a
